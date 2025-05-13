1dd860b0e52da74db4701e13fd8fb874d9163a2a
Evgeniya Vashkevich <evgeniya.vashkevich@suse.com>
2025-05-13 13:20:24 -0700

    set the proider name to predefined value

--- pkg/capi/edit/turtles-capi.cattle.io.capiprovider/ProviderConfig.vue
+++ pkg/capi/edit/turtles-capi.cattle.io.capiprovider/ProviderConfig.vue
@@ -41,6 +41,7 @@ const customProviderSpec = {
 };
 
 const providerTypes = ['infrastructure', 'bootstrap', 'controlPlane', 'addon', 'ipam', 'runtimeextension', 'core'];
+const CUSTOM = 'custom';
 
 export default {
   name:       'ProviderConfig',
@@ -90,7 +91,6 @@ export default {
       loading:            true,
       fvFormRuleSets:          [
         { path: 'metadata.name', rules: ['name'] },
-        { path: 'spec.name', rules: ['required'] },
         { path: 'spec.version', rules: ['version'] },
         { path: 'spec.fetchConfig.url', rules: ['url'] },
       ],
@@ -130,13 +130,13 @@ export default {
       return !!this.value?.spec?.variables;
     },
     isCustom() {
-      return this.provider === 'custom';
+      return this.provider === CUSTOM;
     },
     shouldShowBanner() {
       return this.isEdit && (this.hasFeatures || this.hasVariables);
     },
     waitingForCredential() {
-      return this.credentialComponent && !this.value.spec.credentials?.rancherCloudCredentialNamespaceName;
+      return this.credentialComponent && !this.value?.spec?.credentials?.rancherCloudCredentialNamespaceName;
     },
     rancherCloudCredentialNamespaceName() {
       return this.value.spec?.credentials?.rancherCloudCredentialNamespaceName || '';
@@ -147,9 +147,9 @@ export default {
       if ( !this.value.spec ) {
         const defaultsFromCoreProvider = this.getSpecFromCoreSecret();
 
-        if ( this.provider !== 'custom') {
+        if ( this.provider !== CUSTOM) {
           set(this.value, 'spec', { ...clone(defaultSpec), ...defaultsFromCoreProvider });
-          set(this.value.spec, 'name', this.provider); // Defines the provider kind to provision.
+          set(this.value.metadata, 'name', this.provider);
         } else {
           set(this.value, 'spec', { ...clone(customProviderSpec), ...defaultsFromCoreProvider });
         }
@@ -247,7 +247,7 @@ export default {
     @cancel="done"
     @error="e=>errors=e"
   >
-    <div v-if="isCustom">
+  <div v-if="isCustom">
       <NameNsDescription
         :value="value"
         :mode="mode"
@@ -335,7 +335,7 @@ export default {
     </div>
     <div
       v-if="credentialComponent"
-      class="mb-40"
+      class="mb-10"
     />
     <h2
       v-if="hasFeatures || hasVariables"
